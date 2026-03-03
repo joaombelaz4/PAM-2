f4366b36e8123a43431dff2e689417099c797428
joaombelaz4 <joaombelaz4@gmail.com>
2026-03-03 14:21:37 +0000

    Começou a funfar

--- APP/App.js
+++ APP/App.js
@@ -7,7 +7,7 @@ export default function App() {
     redirect: 'follow'
   };
 
-  fetch("https://literate-waffle-7v5q9gpxxjvghx79v-3000.app.github.dev/", requestOptions)
+  fetch("https://literate-waffle-7v5q9gpxxjvghx79v-3000.app.github.dev/posts", requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
